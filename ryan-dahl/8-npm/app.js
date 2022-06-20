22906a0e553f5e01bba49f9d23687d14e8d35b38
andi saputra <sapoetrandi10@gmail.com>
2022-06-20 15:17:14 +0700

    Belajar NodeJS | 9. Menggunakan Package NPM

--- ryan-dahl/8-npm/app.js
+++ ryan-dahl/8-npm/app.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const validator = require('validator');
+const chalk = require('chalk');
+
+console.log(validator.isEmail("andisaputra@gmail.com"));
+console.log(validator.isMobilePhone("081939292929", "id-ID"));
+console.log(validator.isNumeric("081939292929"));
+console.log("nodemon nyala");
+
+console.log(chalk.black.italic.bgBlue("Hello World!!"));
+const pesan = chalk`Lorem ipsum dolor {bgRed.italic sit amet} consectetur adipisicing elit. Iusto, nesciunt?`;
+console.log(pesan);
