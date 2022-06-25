a2f4fdf13a7f6ff9dd70f40aaacb8c09773fd85b
andi saputra <sapoetrandi10@gmail.com>
2022-06-25 10:48:17 +0700

    Belajar NodeJS | 11. Membuat Contact App (Bagian 2)

--- ryan-dahl/contact-app/app.js
+++ ryan-dahl/contact-app/app.js
@@ -1,11 +1,52 @@
-const { simpanContact, inPertanyaan } = require("./contacts");
+const yargs = require("yargs");
+const contacts = require('./contacts');
 
-const main = async () =>{
-  const nama =  await inPertanyaan('masukan nama anda: ');
-  const email =  await inPertanyaan('masukan email anda: ');
-  const noHp =  await inPertanyaan('masukan No Hp anda: ');
+//cara pertama
+// yargs.command('add', 'menambahkan contact baru', () => {}, (argv) => {
+//   console.log(argv.nama);
+// });
 
-  simpanContact(nama, email, noHp);
-}
+//cara kedua
+yargs.command({
+  command: 'add',
+  describe: 'Menambahkan kontak baru',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    },
+    email: {
+      describe: 'Email',
+      demandOption: false,
+      type: 'string'
+    },
+    noHp: {
+      describe: 'Nomor Handphone',
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler(argv){
+    // const contact = {
+    //   nama: argv.nama,
+    //   email: argv.email,
+    //   noHp: argv.noHp,
+    // };
+    // console.log(contact);
+    contacts.simpanContact(argv.nama, argv.email, argv.noHp)
+  },
+});
 
-main();
+yargs.parse();
+// const { simpanContact, inPertanyaan } = require("./contacts");
+
+// const main = async () =>{
+//   const nama =  await inPertanyaan('masukan nama anda: ');
+//   const email =  await inPertanyaan('masukan email anda: ');
+//   const noHp =  await inPertanyaan('masukan No Hp anda: ');
+
+//   simpanContact(nama, email, noHp);
+// }
+
+// main();
